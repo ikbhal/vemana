71cc4c1f5fa62b054d66cdefb199a5962c6f0032
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-12-19 10:56:09 +0530

    feature list

--- vemana/poems/models.py
+++ vemana/poems/models.py
@@ -6,6 +6,9 @@ class Poem(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=100)
     likes = models.IntegerField(default=0)
+    collected_from = models.CharField(max_length=100, blank=True, null=True)
+    collected_by = models.CharField(max_length=100, blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
