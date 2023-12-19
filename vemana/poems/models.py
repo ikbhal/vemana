baa632c504abba734b76587bd7311aa107e9c2d8
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-12-19 11:07:23 +0530

    english writing, meaning, telugu writing, telugu meaning

--- vemana/poems/models.py
+++ vemana/poems/models.py
@@ -9,6 +9,12 @@ class Poem(models.Model):
     collected_from = models.CharField(max_length=100, blank=True, null=True)
     collected_by = models.CharField(max_length=100, blank=True, null=True)
     note = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+    english_meaning = models.TextField(blank=True, null=True)
+    english_writing = models.TextField(blank=True, null=True)
+    telugu_meaning = models.TextField(blank=True, null=True)
+    telugu_writing = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
